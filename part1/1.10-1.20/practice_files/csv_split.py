d5813c1c55242aeb9d03cd9cdc661d5c34484c83
Peter Marks <peter.marks@gmail.com>
2018-08-24 01:05:14 -0400

    Polished csv_split.py

--- part1/1.10-1.20/practice_files/csv_split.py
+++ part1/1.10-1.20/practice_files/csv_split.py
@@ -37,18 +37,22 @@ def get_args():
   return parser.parse_args()
 
 def validate_input(input_file):
+  # Simple function to verify that the input csv file exists on the system
   return os.path.exists(input_file)
 
 def validate_csv(input_file, row_limit):
+  # Read through the .csv file and ensure it has more rows than the row limit
   with open(input_file, "r") as my_csv_file:
     my_csv = csv.reader(my_csv_file)
     next(my_csv)
     rows = 0
     for row in my_csv:
       rows += 1
+  # Returns true if the number of rows is greater than the row limit (not counting the header)
   return rows > row_limit
 
 def validate_output(output_file):
+  # Ensures the output string does not have .csv at the end, removing if needed
   if output_file.endswith(".csv"):
     print("WARNING: Output filename ends with .csv. Truncating.")
     return output_file[0:len(output_file) - 4]
@@ -56,35 +60,57 @@ def validate_output(output_file):
     return output_file
 
 def write_csv(output_csv, iteration, name):
+  # Output to the screen the file details to be written
   filename = "{}{:03}.csv".format(name, iteration)
   print("Chunk {}: Writing {} rows to {}...".format(iteration, len(output_csv) - 1, filename))
 
+  # Open the target file and write all lines to the file
   with open(filename, "w") as my_output_file:
     my_output_csv = csv.writer(my_output_file)
     my_output_csv.writerows(output_csv)
 
+def csv_init(header):
+  # Create a csv list variable and add the header
+  csv = []
+  csv.append(header)
+  return csv
+
 def split_csv(input_csv, row_limit, output):
+  # Main function to split the csv file
   print("Splitting {} after every {} rows...".format(input_csv, row_limit))
 
+  # Open and read through the input .csv file
   with open(input_csv, "r") as my_input_file:
+    # Open the file and separate the header row
     my_input_csv = csv.reader(my_input_file)
     my_header = next(my_input_csv)
     
-    csv_file = []
-    csv_file.append(my_header)
+    # Create the list for the csv data and add the header
+    csv_file = csv_init(my_header)
 
+    # Initialize variables for loop
     curr_csv = 0
     curr_row = 0
+
+    # Loop through all rows
     for row in my_input_csv:
+      # Append the row and increment the row number
       csv_file.append(row)
       curr_row += 1
+
+      # Check if the row limit has been hit
       if curr_row >= row_limit:
+        # Call the write_csv function
         write_csv(csv_file, curr_csv, output)
+
+        # Delete and recreate the csv file
         del(csv_file)
-        csv_file = []
-        csv_file.append(my_header)
+        csv_file = csv_init(my_header)
+
+        # Increment and reset variables
         curr_csv += 1
         curr_row = 0
+
 #
 # Main Function
 #
@@ -101,7 +127,5 @@ validate_csv(my_args.input, my_args.rows) or die("ERROR: Input .csv file not val
 # Validate output csv name doesn't end with .csv
 good_output = validate_output(my_args.output)
 
-# Split input csv data into a list of lists based on specified rows
-csv_files = split_csv(my_args.input, my_args.rows, good_output)
-
-
+# Main split function, writing the output files along the way
+split_csv(my_args.input, my_args.rows, good_output)
